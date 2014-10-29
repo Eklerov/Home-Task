2e309874dd77feec5642ef47823a3c3777234c14
Dmitry <Eklerov333@gmail.com>
2014-10-29 15:52:45 +0300

    Original

---
+++ untitled/src/com/HomeTask/Fibo.java
@@ -0,0 +1,37 @@
+package com.HomeTask;
+
+/**
+ * Created by Dmitry on 29.10.14.
+ */
+ class Fibo {
+
+
+public static void main(String [] args){
+Fibo f=new Fibo();
+    int i=0;
+    while(i<11){
+        ++i;
+        f.fib(0);
+
+
+        System.out.println(fib(i));
+    }
+
+}
+
+  static int fib(int n) {
+
+          if(n==0){
+              return 0;
+          }
+          if(n==1){
+              return 1;
+          }
+          else {
+              return fib(n-1)+fib(n-2);
+          }
+
+    }
+
+}
+
