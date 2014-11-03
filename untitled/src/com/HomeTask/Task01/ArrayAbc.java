00d72c68d1661b01a8eeba856ed93ba2031fcfb0
Dmitry <Eklerov333@gmail.com>
2014-11-03 16:42:48 +0300

    Original

---
+++ untitled/src/com/HomeTask/Task01/ArrayAbc.java
@@ -0,0 +1,36 @@
+package com.HomeTask.Task01;
+
+public class ArrayAbc
+{
+
+    public static void main(String[] args){
+
+        int [][] Mas = new int[7][4];
+        int [] Mas1 = new int[Mas.length];
+
+        for(int i =0;i<Mas.length;i++){
+
+            for(int h=0;h<Mas[i].length;h++){
+                Mas[i][h]=(int)(Math.random()*11)-5;
+                System.out.print(Mas[i][h]+" ");
+
+                if(h==0)Mas1[i]=Mas[i][h];
+                else Mas1[i]*=Mas[i][h];
+
+                if(h==Mas[i].length-1)
+                    System.out.println(" ");
+            }
+        }
+
+        int max=0,max_i=0;
+
+        for(int i=0;i<Mas1.length;i++){
+            if(Math.abs(Mas1[i])>max){
+                max=Mas1[i];
+                max_i=i;
+            }
+        }
+
+        System.out.println("произведение елементов("+max+") индекс строки - "+max_i);
+    }
+}
